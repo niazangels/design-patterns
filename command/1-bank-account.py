339a0c2a7813eb353f461e5f7a49b54f2eae6eb5
Niyas Mohammed <niaz.angels@gmail.com>
2019-12-20 23:28:31 +0530

    Add Command/Bank Account examples

---
+++ command/1-bank-account.py
@@ -0,0 +1,88 @@
+from abc import ABC
+from enum import Enum
+
+
+class BankAccount:
+    OVERDRAFT_LIMIT = -500
+
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}, balance={self.balance}")
+
+    def withdraw(self, amount):
+        if self.balance - amount < self.OVERDRAFT_LIMIT:
+            return False
+        self.balance -= amount
+        print(f"Withdrew {amount}, balance={self.balance}")
+        return True
+
+    def __str__(self):
+        return f"Balance={self.balance}"
+
+
+class Command(ABC):
+    def invoke(self):
+        pass
+
+    def undo(self):
+        pass
+
+
+class BankAccountCommand(Command):
+    def __init__(self, account, action, amount):
+        self.account = account
+        self.amount = amount
+        self.action = action
+        self.success = None
+
+    class Action(Enum):
+        DEPOSIT = 0
+        WITHDRAW = 1
+
+    def invoke(self):
+        if self.action == self.Action.DEPOSIT:
+            self.account.deposit(self.amount)
+            self.success = True
+        elif self.action == self.Action.WITHDRAW:
+            self.success = self.account.withdraw(self.amount)
+
+    def undo(self):
+        if not self.success:
+            return
+        if self.action == self.Action.WITHDRAW:
+            self.account.deposit(self.amount)
+        elif self.action == self.Action.DEPOSIT:
+            self.account.withdraw(self.amount)
+
+
+if __name__ == "__main__":
+    ba = BankAccount()
+    print("> Init")
+    print(ba)
+
+    print("> Add +100")
+    cmd = BankAccountCommand(ba, BankAccountCommand.Action.DEPOSIT, 100)
+    cmd.invoke()
+    print(ba)
+
+    print("> Undo")
+    cmd.undo()
+    print(ba)
+
+    # Bug: undo is repeatable
+    print("> Fail: Undo once again")
+    cmd.undo()
+    print(ba)
+
+    print("> Withdraw 500")
+    illegal_cmd = BankAccountCommand(ba, BankAccountCommand.Action.WITHDRAW, 500)
+    illegal_cmd.invoke()
+    print(ba)
+
+    print("> Withdraw too much")
+    illegal_cmd = BankAccountCommand(ba, BankAccountCommand.Action.WITHDRAW, 5000)
+    illegal_cmd.invoke()
+    print(ba)
